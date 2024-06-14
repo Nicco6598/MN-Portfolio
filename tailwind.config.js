ec25bc3fb8a3e3704c5631adab83d3432ec40ee5
Nicco6598 <nicco6598@gmail.com>
2024-06-14 10:39:11 +0200

    aggiornamento colori

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         'murrey': '#FF6F61',      // Un rosso corallo vivace
         'dark-cyan': '#00B4D8',   // Un azzurro vibrante
         'flax': '#FFDA77',        // Un giallo solare e acceso
-        'alice-blue': '#cfe3e6',  // Un turchese chiaro e fresco
+        'alice-blue': '#f0fcff',  // Un azzurro chiaro e fresco
         'rich-black': '#212121',  // Un nero intenso con un tocco di modernità
       },
       fontFamily: {
