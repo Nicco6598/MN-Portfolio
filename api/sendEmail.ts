99c1bf3e2530673b6dbaa5e05ca429ea739c7187
Nicco6598 <nicco6598@gmail.com>
2024-06-17 18:27:00 +0200

    push gmail smtp

--- api/sendEmail.ts
+++ api/sendEmail.ts
@@ -1,23 +1,25 @@
-// api/sendEmail.js
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 async function sendEmail(req, res) {
   const { email, message, selectedProject } = req.body;
 
+  // Configura il trasportatore SMTP di Nodemailer
   let transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
+    secure: false, // true per 465, false per altre porte
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
   });
 
+  // Configura le opzioni dell'email
   const mailOptions = {
-    from: `"Your Name" <${process.env.SMTP_USER}>`,
+    from: `"Portfolio Modulo Contatti" <${process.env.SMTP_USER}>`,
     to: 'nicco6598@gmail.com', // Cambia con la tua email
-    subject: 'Nuovo messaggio contatto dal Portfolio',
+    subject: 'Nuovo messaggio dal sito web',
     text: `Email: ${email}\n\nMessaggio:\n${message}\n\nProgetto Selezionato: ${selectedProject || 'Nessun progetto selezionato'}`,
   };
 
